bbbda4cf0cb3d88b4c681ef1d94b7ecfee7d1b09
Steven Ruiz <sarr1998@hotmail.com>
2024-02-15 19:25:52 -0500

    Bitacora Procesador Archivos Spring

---
+++ bitacoraprocesador/bitacoraprocesador/src/test/java/co/edu/uco/bitacoraprocesador/BitacoraprocesadorApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.bitacoraprocesador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitacoraprocesadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
